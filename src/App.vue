024fdbd16e8d1c759a4bdde4867347e2bfacfd21
Igor <Bala6@LaptopvonLeyla.fritz.box>
2026-06-28 23:32:10 +0200

    Header with route security was created

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { useUserStore } from './stores/counter';
 import Toast from 'primevue/toast'
+import Header from './components/Header.vue'
+
+const authStore = useUserStore()
+
+onMounted( async() => {
+  await authStore.getUser()
+})
+
 </script>
 
 <template>
+  <Header v-if="authStore.user"/>
   <Toast position="top-right" />
   <router-view />
 </template>
-
-<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+    import Button from 'primevue/button';
+    import Avatar from 'primevue/avatar';
+    import Menubar from 'primevue/menubar';
+    import { computed } from 'vue';
+    import { useUserStore } from '@/stores/counter';
+
+    const authStore = useUserStore()
+
+    const emailFirstLetter = computed(() => {
+      const letter = authStore.user?.email?.[0]
+      return letter ? letter.toUpperCase() : ''
+    })
+
+</script>
+
+<template>
+    <div class="mb-5">
+        <Menubar>
+            <template #start>
+                <div class="flex items-center gap-2">
+                    <span class="font-bold">Link Manager</span>
+                    <div class="flex items-center gap-2">
+                        <Button icon="pi pi-link" rounded />
+                        <Button icon="pi pi-folder" rounded />
+                    </div>
+                </div>
+            </template>
+            <template #end>
+                <div class="flex items-center gap-2">
+                    <Avatar :label="emailFirstLetter" size="large" shape="circle" />
+                    <Button icon="pi pi-sign-out" rounded severity="secondary" />
+                </div>
+            </template>
+        </Menubar>
+    </div>
+</template>
